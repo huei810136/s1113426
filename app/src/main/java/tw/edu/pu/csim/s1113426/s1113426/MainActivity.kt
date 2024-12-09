84643b820e6517ea3813bf8f972624e6f80601a2
huei810136 <ssss9914@gmail.com>
2024-12-09 14:26:13 +0800

    v0_林彗靖

---
+++ app/src/main/java/tw/edu/pu/csim/s1113426/s1113426/MainActivity.kt
@@ -0,0 +1,50 @@
+package tw.edu.pu.csim.s1113426.s1113426
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import tw.edu.pu.csim.s1113426.s1113426.ui.theme.S1113426Theme
+import android.content.pm.ActivityInfo
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            S1113426Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT)
+
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    S1113426Theme {
+        Greeting("Android")
+    }
+}
